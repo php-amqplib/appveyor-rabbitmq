6919d8b4ed6ca40618a16c773f91c7b4cdb083f1
Ramūnas Dronga <github@ramuno.lt>
2019-11-04 09:40:52 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,15 @@
 Write-Output "Removing previous erlang versions..."
 Get-ChildItem -Path 'C:\Program Files\erl*\Uninstall.exe' | %{ Start-Process -Wait -NoNewWindow -FilePath $_ -ArgumentList '/S' }
 refreshenv
+# remove inherited erlang path
+Remove-Item Env:\ERLANG_HOME
 
 Write-Output "Creating erlang cookie..."
 [System.IO.File]::WriteAllText("C:\Users\appveyor\.erlang.cookie", "rabbitmq", [System.Text.Encoding]::ASCII)
 [System.IO.File]::WriteAllText("C:\Windows\System32\config\systemprofile\.erlang.cookie", "rabbitmq", [System.Text.Encoding]::ASCII)
 
 Write-Output "Installing RabbitMQ..."
-choco install rabbitmq -y --no-pogress --debug --trace --version=$env:RABBITMQ_VERSION
+choco install rabbitmq -y --no-pogress --trace --version=$env:RABBITMQ_VERSION
 refreshenv
 
 Invoke-WebRequest "https://raw.githubusercontent.com/pika/pika/master/testdata/wait-epmd.ps1" -OutFile "wait-epmd.ps1"
